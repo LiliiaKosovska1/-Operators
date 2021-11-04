83d42b2cd12fa86922d8764dcb515b9ffe0d7bd4
Liliia Kosovska <liliia.kosovska@globallogic.com>
2021-11-04 15:46:20 +0200

    edited conditions

--- conditions.js
+++ conditions.js
@@ -1,1 +1,35 @@
-console.log("test");
+//square of triangle
+const a = 5;
+const b = 10;
+
+let result;
+
+result = a > 0 && b > 0
+    ? (a * b) / 2
+    : "it's not a triangle";
+console.log(result);
+
+//Switch case
+const color = 5;
+switch (color){
+    case 1: {
+         console.log('Red');
+    }
+    break;
+    case 2: {
+        console.log('Blue')
+    }
+    break;
+    case 3: {
+         console.log('Yellow');
+    }
+    break;
+    case 4: {
+        console.log('Green')
+    }
+    break;
+    default:{
+        console.error('Wrong color!')
+    }
+}
+
